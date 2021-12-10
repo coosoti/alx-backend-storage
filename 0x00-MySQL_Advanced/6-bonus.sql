63d55f0e3a6be3500b8ab499043bb6f096415e89
coosoti <charlesosoti@gmail.com>
2021-12-10 09:00:36 +0300

    refactor

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -10,10 +10,11 @@ CREATE PROCEDURE AddBonus (
     IN score float
 )
 BEGIN
-    IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
-		INSERT INTO projects (name) VALUES (project_name);
-	END IF;
-	INSERT INTO corrections (user_id, project_id, score)
-	VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
+    INSERT INTO projects (name)
+    SELECT project_name FROM DUAL
+
+    WHERE NOT EXISTS (SELECT * FROM projects WHERE name = project_name);
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
 END;
 |
